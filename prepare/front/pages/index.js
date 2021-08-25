139011ea1a238de483ed0dac5ee1aaa0084ff1f9
hippo97 <83958296+hippo97@users.noreply.github.com>
2021-08-26 01:43:56 +0900

    Initial commit

--- prepare/front/pages/index.js
+++ prepare/front/pages/index.js
@@ -3,7 +3,9 @@ import AppLayout from "../Components/AppLayout";
 
 const Home = () => {
     return (
-        <AppLayout><div>Hello, Next!</div></AppLayout>
+        <AppLayout>
+            <div>Hello, Next!</div>
+        </AppLayout>
     );
 };
 
